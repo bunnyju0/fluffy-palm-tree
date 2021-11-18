333a7ee598faab0bca36afc807f67777bb9a5809
bunnyju0 <92170129+bunnyju0@users.noreply.github.com>
2021-11-18 16:34:28 +0900

    Add files via upload

---
+++ syme-web/subpage/apply/javascript/apply.js
@@ -0,0 +1,22 @@
+$(function(){
+    
+        const menuopen = $(".menu-bar-open");   
+        const menuclose = $(".menu-bar-close");
+        const menuList = $(".menu-bar-pc");
+
+
+
+        menuopen.click(function(){
+            menuList.stop().animate({left: "0%"},0);
+            menuopen.hide();
+            menuclose.show();
+        });
+
+        menuclose.click(function(){
+            menuList.stop().animate({left: "110%"},500);
+            menuopen.show();
+            menuclose.hide();
+
+        });
+      
+});
