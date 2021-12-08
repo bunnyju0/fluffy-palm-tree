8ded142c9bac7a29046ecefe126266cc60baea10
bunnyju0 <92170129+bunnyju0@users.noreply.github.com>
2021-12-08 22:07:59 +0900

    Add files via upload

---
+++ port1/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 포트폴리오</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <header>
+        <h1>로고</h1>
+    </header>
+    <section>
+        <h2>글 목록</h2>
+            <ul class="table">
+                <li>
+                    <a href="#">
+                        <span>글 순서</span>
+                        <div c>
+                            <h3>글제목</h3>
+                            <ul class="data">
+                                <li>이름</li>
+                                <li>시간</li>
+                                <li>조회수</li>
+                            </ul>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span>글 순서</span>
+                        <div>
+                            <h3>글제목</h3>
+                            <ul class="data">
+                                <li>이름</li>
+                                <li>시간</li>
+                                <li>조회수</li>
+                            </ul>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span>글 순서</span>
+                        <div>
+                            <h3>글제목</h3>
+                            <ul class="data">
+                                <li>이름</li>
+                                <li>시간</li>
+                                <li>조회수</li>
+                            </ul>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+            <a href="#">글쓰기</a>
+    </section>    
+
+    <footer>
+
+    </footer>
+</body>
+</html>
